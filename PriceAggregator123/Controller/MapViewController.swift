e386d5cb436790e5d9ad06b8ce974f6e73ca64c9
Vlad Silchenko <kyvinept@gmail.com>
2018-08-21 12:30:41 +0300

    modify google map

--- PriceAggregator123/Controller/MapViewController.swift
+++ PriceAggregator123/Controller/MapViewController.swift
@@ -11,7 +11,6 @@ import CoreLocation
 import SwiftyJSON
 import GoogleMaps
 import GooglePlaces
-import GooglePlacePicker
 
 class MapViewController: UIViewController {
     
@@ -39,35 +38,10 @@ class MapViewController: UIViewController {
 
 extension MapViewController: GMSMapViewDelegate{
     func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
-        print(marker.title)
-        print(marker.description)
-        let northEast = CLLocationCoordinate2D(latitude: marker.position.latitude, longitude: marker.position.longitude)
-        let southWest = CLLocationCoordinate2D(latitude: marker.position.latitude, longitude: marker.position.longitude)
-        let viewport = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
-        let config = GMSPlacePickerConfig(viewport: viewport)
-        let placePicker = GMSPlacePicker(config: config)
-        placePicker.pickPlace(callback: {(place, error) -> Void in
-            if let error = error {
-                print("Pick Place error: \(error.localizedDescription)")
-                return
-            }
-            if let place = place {
-                let alert = UIAlertController(title: place.name, message: place.formattedAddress?.components(separatedBy: ", ").joined(separator: "\n"), preferredStyle: .alert)
-                let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default) { (action) in
-                    self.dismiss(animated: true, completion: nil)
-                }
-                alert.addAction(action)
-                self.present(alert, animated: true, completion: nil)
-            } else {
-                let alert = UIAlertController(title: "No place selected", message: "", preferredStyle: .alert)
-                let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default) { (action) in
-                    self.dismiss(animated: true, completion: nil)
-                }
-                alert.addAction(action)
-                self.present(alert, animated: true, completion: nil)
-            }
-        })
-        return UIView()
+        let info = Bundle.main.loadNibNamed("InfoPlace", owner: nil, options: nil)?.first as! InfoPlace
+        info.setValues(name: marker.title!, address: marker.userData as! String, phoneNumber: marker.snippet!)
+        info.frame.size = CGSize(width: 200, height: 70)
+        return info
     }
 }
 
@@ -87,14 +61,6 @@ extension MapViewController: CLLocationManagerDelegate {
         }
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
-    }
-
-    func showMarker(position: CLLocationCoordinate2D){
-        let marker = GMSMarker()
-        marker.position = position
-        marker.title = "Palo Alto"
-        marker.snippet = "San Francisco"
-        marker.map = mapView
     }
     
     func getShops(latitude: String, longitude: String) {
@@ -125,74 +91,10 @@ extension MapViewController: CLLocationManagerDelegate {
             let marker = GMSMarker()
             marker.position = CLLocationCoordinate2D(latitude: json[i]["coordinates"][1].double!, longitude: json[i]["coordinates"][0].double!)
             marker.title = json[i]["name"].string!
+            marker.snippet = json[i]["phoneNumber"].string!
+            marker.userData = json[i]["streetAddress"].string!
             marker.map = mapView
             i += 1
         }
     }
 }
-
-//    @IBOutlet weak var mapkit: MKMapView!
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        manager.delegate = self
-//        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
-//        manager.requestAlwaysAuthorization()
-//        title = "Map"
-//    }
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        manager.requestLocation()
-//    }
-//}
-
-//extension MapViewController: CLLocationManagerDelegate {
-//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-//        let coordinateRegion = MKCoordinateRegionMakeWithDistance(locations.first!.coordinate, 100_000, 100_000)
-//        mapkit.setRegion(coordinateRegion, animated: true)
-//        if let location = locations.first {
-//            getShops(latitude: String(location.coordinate.latitude), longitude: String(location.coordinate.longitude))
-//        }
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
-//        let alert = UIAlertController(title: "Error", message: "You need to allow identify location that we can show shops near you.", preferredStyle: .alert)
-//        let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default) { (action) in
-//            self.dismiss(animated: true, completion: nil)
-//        }
-//        alert.addAction(action)
-//        self.present(alert, animated: true, completion: nil)
-//    }
-//
-//    func getShops(latitude: String, longitude: String) {
-//        guard let url = URL(string: "http://api.walmartlabs.com/v1/stores?format=json&lat=\(latitude)&lon=\(longitude)&apiKey=jx9ztwc42y6mfvvhfa4y87hk") else { return }
-//        let session = URLSession.shared
-//        session.dataTask(with: url) { (data, responce, error) in
-//            do {
-//                let json = try JSON(data: data!)
-//                if json.isEmpty {
-//                    let alert = UIAlertController(title: "No shops", message: "There are no shops in your area.", preferredStyle: .alert)
-//                    let action = UIAlertAction(title: "Ok", style: UIAlertActionStyle.default) { (action) in
-//                        self.dismiss(animated: true, completion: nil)
-//                    }
-//                    alert.addAction(action)
-//                    self.present(alert, animated: true, completion: nil)
-//                    return
-//                }
-//                self.setCoordinatesFromJSON(json: json)
-//            } catch { }
-//        }.resume()
-//    }
-//
-//    func setCoordinatesFromJSON(json: JSON) {
-//        var i = 0
-//        while json[i] != JSON.null {
-//            let annotation = MKPointAnnotation()
-//            annotation.coordinate = CLLocationCoordinate2D(latitude: json[i]["coordinates"][1].double!, longitude: json[i]["coordinates"][0].double!)
-//            annotation.title = json[i]["name"].string!
-//            mapkit.addAnnotation(annotation)
-//            i += 1
-//        }
-//    }
-//}
